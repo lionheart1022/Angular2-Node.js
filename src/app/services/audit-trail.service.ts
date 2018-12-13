386fcb89ea3ab7c2ad12f2e15dde8e94691acf59
Oleksii Zamiatin <alex.zamiatin910@gmail.com>
2018-12-13 12:54:10 +0200

    updated audit trail service

--- src/app/services/audit-trail.service.ts
+++ src/app/services/audit-trail.service.ts
@@ -15,13 +15,50 @@ export class AuditTrailService {
 
   constructor(private http: HttpClient) { }
 
-  getAuditTrail({ pageNum, pageSize }: any): Observable<AuditTrailResponse> {
+  getAuditByUsername(userName: string, pageNum: number, pageSize:number): Observable<AuditTrailResponse> {
     let headers = new HttpHeaders();
-    headers.append('content-type', 'application/json')
+    headers.append('Content-Type', 'application/json');
 
-    return this.http.get(`${config.url}audittrail?pagenum=${pageNum}&pagesize=${pageSize}`, { headers: headers })
+    if (userName == 'all users') {
+      return this.http.get(`${config.url}audittrail?pagenum=${pageNum}&pagesize=${pageSize}`, { headers: headers })
               .map(response => response)
               .catch(this.handleError);
+    }
+
+    return this.http.get(`${config.url}audittrail/${userName}?pagenum=${pageNum}&pagesize=${pageSize}`, { headers: headers })
+            .map(response => response)
+            .catch(this.handleError);   
+  }
+
+  getAuditByPeriod(dateFrom: string, dateTo: string, userName: string, pageNum: number, pageSize:number): Observable<AuditTrailResponse> {
+    let headers = new HttpHeaders();
+    // headers.append('Host', 'localhost:8080');
+    headers.append('Content-Type', 'application/json');
+
+    let payload = {}
+
+    if (userName == 'all users') {
+      payload = {
+        'from': dateFrom,
+        'to': dateTo,
+        'pageNum': pageNum,
+        'pageSize': pageSize
+      };  
+    }
+
+    else {
+      payload = {
+        'from': dateFrom,
+        'to': dateTo,
+        'pageNum': pageNum,
+        'pageSize': pageSize,
+        'username': userName
+      };
+    }
+
+    return this.http.post(`${config.url}audittrail/period`, payload, { headers: headers })
+            .map(response => response)
+            .catch(this.handleError);
   }
 
   private handleError(error:any) {
